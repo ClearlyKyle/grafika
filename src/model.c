fb0c8a37c1df8cb8e278b69d3dae48ecbd56d5d7
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-07-07 22:02:16 +0100

    moved functions

--- src/model.c
+++ src/model.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include "grafika.h"
 #include "tex.h"
 #include "obj.h"
 #include "timer.h"
+#include "matematika.h"
 
-#define max(a, b) (((a) > (b)) ? (a) : (b))
-#define min(a, b) (((a) < (b)) ? (a) : (b))
-
-typedef float vec3[3];
-typedef float vec4[4];
-typedef vec4  mat4[4];
-typedef vec3  triangle[3];
+typedef vec3 triangle[3];
 
 typedef struct rasterstate
 {
@@ -29,222 +23,9 @@ static rasterstate_t state = {0};
 
 static float zbuffer[GRAFIKA_SCREEN_HEIGHT * GRAFIKA_SCREEN_WIDTH];
 
-static inline float v3dot(const vec3 v0, const vec3 v1)
-{
-    return (v0[0] * v1[0] + v0[1] * v1[1] + v0[2] * v1[2]);
-}
-
-static inline void v3sub(const vec3 v0, const vec3 v1, vec3 res)
-{
-    res[0] = v0[0] - v1[0];
-    res[1] = v0[1] - v1[1];
-    res[2] = v0[2] - v1[2];
-}
-
-static inline void v3div(const vec3 v, const float val, vec3 res)
-{
-    res[0] = v[0] / val;
-    res[1] = v[1] / val;
-    res[2] = v[2] / val;
-}
-
 static inline float edgefunc(const vec3 a, const vec3 b, const vec3 c)
 {
     return (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]);
-}
-
-static inline void m4mulm4(const mat4 m1, const mat4 m2, mat4 dest)
-{
-    float a00 = m1[0][0], a01 = m1[0][1], a02 = m1[0][2], a03 = m1[0][3],
-          a10 = m1[1][0], a11 = m1[1][1], a12 = m1[1][2], a13 = m1[1][3],
-          a20 = m1[2][0], a21 = m1[2][1], a22 = m1[2][2], a23 = m1[2][3],
-          a30 = m1[3][0], a31 = m1[3][1], a32 = m1[3][2], a33 = m1[3][3],
-
-          b00 = m2[0][0], b01 = m2[0][1], b02 = m2[0][2], b03 = m2[0][3],
-          b10 = m2[1][0], b11 = m2[1][1], b12 = m2[1][2], b13 = m2[1][3],
-          b20 = m2[2][0], b21 = m2[2][1], b22 = m2[2][2], b23 = m2[2][3],
-          b30 = m2[3][0], b31 = m2[3][1], b32 = m2[3][2], b33 = m2[3][3];
-
-    dest[0][0] = a00 * b00 + a10 * b01 + a20 * b02 + a30 * b03;
-    dest[0][1] = a01 * b00 + a11 * b01 + a21 * b02 + a31 * b03;
-    dest[0][2] = a02 * b00 + a12 * b01 + a22 * b02 + a32 * b03;
-    dest[0][3] = a03 * b00 + a13 * b01 + a23 * b02 + a33 * b03;
-    dest[1][0] = a00 * b10 + a10 * b11 + a20 * b12 + a30 * b13;
-    dest[1][1] = a01 * b10 + a11 * b11 + a21 * b12 + a31 * b13;
-    dest[1][2] = a02 * b10 + a12 * b11 + a22 * b12 + a32 * b13;
-    dest[1][3] = a03 * b10 + a13 * b11 + a23 * b12 + a33 * b13;
-    dest[2][0] = a00 * b20 + a10 * b21 + a20 * b22 + a30 * b23;
-    dest[2][1] = a01 * b20 + a11 * b21 + a21 * b22 + a31 * b23;
-    dest[2][2] = a02 * b20 + a12 * b21 + a22 * b22 + a32 * b23;
-    dest[2][3] = a03 * b20 + a13 * b21 + a23 * b22 + a33 * b23;
-    dest[3][0] = a00 * b30 + a10 * b31 + a20 * b32 + a30 * b33;
-    dest[3][1] = a01 * b30 + a11 * b31 + a21 * b32 + a31 * b33;
-    dest[3][2] = a02 * b30 + a12 * b31 + a22 * b32 + a32 * b33;
-    dest[3][3] = a03 * b30 + a13 * b31 + a23 * b32 + a33 * b33;
-}
-
-static inline void m4mulv4(const mat4 m, const vec4 v, vec4 res)
-{
-    res[0] = m[0][0] * v[0] + m[1][0] * v[1] + m[2][0] * v[2] + m[3][0] * v[3];
-    res[1] = m[0][1] * v[0] + m[1][1] * v[1] + m[2][1] * v[2] + m[3][1] * v[3];
-    res[2] = m[0][2] * v[0] + m[1][2] * v[1] + m[2][2] * v[2] + m[3][2] * v[3];
-    res[3] = m[0][3] * v[0] + m[1][3] * v[1] + m[2][3] * v[2] + m[3][3] * v[3];
-}
-
-void normalize(vec3 v)
-{
-    float length = sqrtf(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-
-    if (length == 0.0f)
-    {
-        v[0] = v[1] = v[2] = 0.0f;
-        return;
-    }
-
-    v[0] /= length;
-    v[1] /= length;
-    v[2] /= length;
-}
-
-void cross(const vec3 v1, const vec3 v2, vec3 res)
-{
-    res[0] = v1[1] * v2[2] - v1[2] * v2[1];
-    res[1] = v1[2] * v2[0] - v1[0] * v2[2];
-    res[2] = v1[0] * v2[1] - v1[1] * v2[0];
-}
-
-void subtract(const vec3 v1, const vec3 v2, vec3 res)
-{
-    res[0] = v1[0] - v2[0];
-    res[1] = v1[1] - v2[1];
-    res[2] = v1[2] - v2[2];
-}
-
-void v3add(const vec3 v1, const vec3 v2, vec3 res)
-{
-    res[0] = v1[0] + v2[0];
-    res[1] = v1[1] + v2[1];
-    res[2] = v1[2] + v2[2];
-}
-
-static inline void m4identity(mat4 m)
-{
-    memset(m, 0, sizeof(mat4));
-    m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1.0f;
-}
-
-void rotateMatrix(mat4 matrix, float angle, char axis)
-{
-    m4identity(matrix);
-
-    const float c = cosf(angle);
-    const float s = sinf(angle);
-
-    switch (axis)
-    {
-    case 'x':
-    case 'X':
-        matrix[1][1] = c;
-        matrix[1][2] = -s;
-        matrix[2][1] = s;
-        matrix[2][2] = c;
-        break;
-
-    case 'y':
-    case 'Y':
-        matrix[0][0] = c;
-        matrix[0][2] = s;
-        matrix[2][0] = -s;
-        matrix[2][2] = c;
-        break;
-
-    case 'z':
-    case 'Z':
-        matrix[0][0] = c;
-        matrix[0][1] = -s;
-        matrix[1][0] = s;
-        matrix[1][1] = c;
-        break;
-
-    default:
-        // Invalid axis specified
-        break;
-    }
-}
-
-void m4lookat(const vec3 eye, const vec3 dir, const vec3 up, mat4 res)
-{
-    vec3 target;
-    v3add(eye, dir, target);
-
-    vec3 forward, right, new_up;
-
-    subtract(target, eye, forward);
-    normalize(forward);
-
-    cross(forward, up, right);
-    normalize(right);
-
-    cross(right, forward, new_up);
-    // normalize(new_up);
-
-    res[0][0] = right[0];
-    res[0][1] = new_up[0];
-    res[0][2] = -forward[0];
-    res[0][3] = 0.0f;
-
-    res[1][0] = right[1];
-    res[1][1] = new_up[1];
-    res[1][2] = -forward[1];
-    res[1][3] = 0.0f;
-
-    res[2][0] = right[2];
-    res[2][1] = new_up[2];
-    res[2][2] = -forward[2];
-    res[2][3] = 0.0f;
-
-    res[3][0] = -v3dot(right, eye);
-    res[3][1] = -v3dot(new_up, eye);
-    res[3][2] = v3dot(forward, eye);
-    res[3][3] = 1.0f;
-}
-
-static inline void m4transmake(float x, float y, float z, mat4 res)
-{
-    m4identity(res);
-    res[3][0] = x;
-    res[3][1] = y;
-    res[3][2] = z;
-}
-
-static inline void m4scalemake(float x, float y, float z, mat4 res)
-{
-    m4identity(res);
-    res[0][0] = x;
-    res[1][1] = y;
-    res[2][2] = z;
-}
-
-static inline void m4perspective(float fovy, float aspect, float nearZ, float farZ, mat4 res)
-{
-    memset(res, 0, sizeof(mat4));
-
-    const float f  = 1.0f / tanf(fovy * 0.5f);
-    const float fn = 1.0f / (nearZ - farZ);
-
-#if 1 // [-1, 1]
-    res[0][0] = f / aspect;
-    res[1][1] = f;
-    res[2][2] = (nearZ + farZ) * fn;
-    res[2][3] = -1.0f;
-    res[3][2] = 2.0f * nearZ * farZ * fn;
-#else // [0, 1]
-    res[0][0] = f / aspect;
-    res[1][1] = f;
-    res[2][2] = farZ * fn;
-    res[2][3] = -1.0f;
-    res[3][2] = nearZ * farZ * fn;
-#endif
 }
 
 void drawtriangle(const triangle t, const vec3 texcoords[3], const mat4 MVP)
@@ -373,10 +154,6 @@ void drawtriangle(const triangle t, const vec3 texcoords[3], const mat4 MVP)
     }
 }
 
-#define _PI          3.14159265358979323846264338327950288 /* pi */
-#define _PIf         ((float)_PI)
-#define DEG2RAD(DEG) ((DEG)*_PIf / 180.0f)
-
 static inline void update(void)
 {
     mat4 MVP;
@@ -419,11 +196,14 @@ int main(int argc, char *argv[])
     //  state.obj = obj_load("plane.obj");
     //  state.obj = obj_load("bunny.obj");
     // state.obj = obj_load("res/Dog House/Doghouse.obj");
-    state.obj = obj_load("res/Lorry/lorry.obj");
+    // state.obj = obj_load("res/Lorry/lorry.obj");
+    state.obj = obj_load("res/Camera/Camera.obj");
 
     // state.tex = tex_load("res/Dog House/Doghouse_PBR_BaseColor.png", true);
-    state.tex = tex_load("res/wood.png", false);
-    //   state.tex = tex_load("metal.png", false);
+    // state.tex = tex_load("res/Lorry/texture_atlas.png", true);
+    state.tex = tex_load("res/Camera/texture_atlas.png", true);
+    // state.tex = tex_load("res/wood.png", false);
+    //    state.tex = tex_load("metal.png", false);
 
     // Calculate model height
     BoundingBox_t bbox    = state.obj.bbox;
