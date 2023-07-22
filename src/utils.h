7998e4331c891415df283b67fcd517cd617c2b9b
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-07-22 21:41:10 +0100

    STATIC_ASSERT

--- src/utils.h
+++ src/utils.h
@@ -2,14 +2,14 @@
 #define __UTILS_H__
 
 #ifndef NDEBUG
-#    define ASSERT(EXPR, ...)                                                   \
+    #define ASSERT(EXPR, ...)                                                   \
         if (!(EXPR))                                                            \
         {                                                                       \
             fprintf(stderr, "[ASSERT] %s %s:%d - ", #EXPR, __FILE__, __LINE__); \
             fprintf(stderr, __VA_ARGS__);                                       \
             abort();                                                            \
         }
-#    define SAFE_FREE(POINTER_TO_DATA)    \
+    #define SAFE_FREE(POINTER_TO_DATA)    \
         {                                 \
             if ((POINTER_TO_DATA))        \
             {                             \
@@ -17,9 +17,20 @@
                 (POINTER_TO_DATA) = NULL; \
             }                             \
         }
+
+    #define STATIC_ASSERT(CONDITION, MSG)            \
+        {                                            \
+            typedef char test[(CONDITION) ? 1 : -1]; \
+            (void)(test *)#MSG;                      \
+        }                                            \
+        (void)0
+
+    #define LOG(MSG, ...) fprintf(stderr, MSG, __VA_ARGS__)
 #else
-#    define ASSERT(EXPR, ...)
-#    define SAFE_FREE(POINTER_TO_DATA)
+    #define ASSERT(EXPR, ...)
+    #define SAFE_FREE(POINTER_TO_DATA)
+    #define STATIC_ASSERT(CONDITION, MSG)
+    #define LOG(MSG, ...)
 #endif
 
 #endif // __UTILS_H__
