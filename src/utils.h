29bf45b014557ca025e66490b3571fd04eafe2bb
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2025-03-28 21:16:09 +0000

    logging

--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,9 @@
 #define UNUSED(VAR)      ((void)(VAR))
 #define IS_POWER_OF_2(x) ((x) > 0 && ((x) & ((x)-1)) == 0)
 
+#define KILOBYTES(number) ((number)*1024ull)
+#define MEGABYRES(number) (KILOBYTES(number) * 1024ull)
+
 #define SAFE_FREE(POINTER_TO_DATA)    \
     {                                 \
         if ((POINTER_TO_DATA))        \
@@ -15,34 +18,72 @@
             (POINTER_TO_DATA) = NULL; \
         }                             \
     }
+
+// Log levels
+typedef enum
+{
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_ERROR
+} log_level_t;
+
+// Helper macro to get the log level as a string
+#define LOG_LEVEL_STR(level) \
+    (level == LOG_LEVEL_INFO ? "INFO" : (level == LOG_LEVEL_WARN ? "WARN" : (level == LOG_LEVEL_ERROR ? "ERROR" : "UNKNOWN")))
+
 #define ATTRIBATE_FORMAT_PRINTF(VAL1, VAL2) __attribute__((format(gnu_printf, (VAL1), (VAL2))))
 
-static inline void _log_printf(const char *fmt, ...) ATTRIBATE_FORMAT_PRINTF(1, 2);
-static inline void _log_printf(const char *fmt, ...)
+#ifndef NDEBUG
+static inline void _log_printf(log_level_t level, const char *file, int line, const char *fmt, ...) ATTRIBATE_FORMAT_PRINTF(4, 5);
+static inline void _log_printf(log_level_t level, const char *file, int line, const char *fmt, ...)
 {
-    va_list va_args;
-    va_start(va_args, fmt);
-    {
-        fprintf(stdout, "[LOG] ");
-        vfprintf(stdout, fmt, va_args);
-    }
-    fflush(stdout);
-    va_end(va_args);
+    va_list args;
+    va_start(args, fmt);
+
+    // fprintf(stdout, "[%s] [%s] (%s:%d) ", time_str, LOG_LEVEL_STR(level), file, line);
+    fprintf(stdout, "[%s](%s:%d) ", LOG_LEVEL_STR(level), file, line);
+
+    // Print the actual log message
+    vfprintf(stdout, fmt, args);
+
+    fflush(stdout); // Flush to ensure the output is immediate
+    va_end(args);
 }
 
-#ifndef NDEBUG
-#define LOG(...) _log_printf(__VA_ARGS__)
-// #define LOG(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
-#define ASSERT(EXP, ...)                \
-    if (!(EXP))                         \
-    {                                   \
-        LOG(__VA_ARGS__); \
-        assert(EXP);                    \
-    }
+    #define LOG(level, fmt, ...) _log_printf(level, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+    #define LOG_INFO(fmt, ...)   LOG(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+    #define LOG_WARN(fmt, ...)   LOG(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
+    #define LOG_ERROR(fmt, ...)  LOG(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+
+    #define ASSERT(expr)                                          \
+        do                                                        \
+        {                                                         \
+            if (!(expr))                                          \
+            {                                                     \
+                fprintf(stderr, "Assertion failed: %s\n", #expr); \
+                abort();                                          \
+            }                                                     \
+        } while (0)
+
+    #define ASSERTM(expr, fmt, ...)                               \
+        do                                                        \
+        {                                                         \
+            if (!(expr))                                          \
+            {                                                     \
+                fprintf(stderr, "Assertion failed: %s\n", #expr); \
+                LOG(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__);         \
+                abort();                                          \
+            }                                                     \
+        } while (0)
+
 #else
-#define ASSERT(EXP, MSG, ...)
-#define STATIC_ASSERT(CONDITION, MSG)
-#define LOG(MSG, ...)
+    #define LOG(level, fmt, ...)
+    #define LOG_INFO(fmt, ...)
+    #define LOG_WARN(fmt, ...)
+    #define LOG_ERROR(fmt, ...)
+
+    #define ASSERT(expr)
+    #define ASSERTM(expr, fmt, ...)
 #endif
 
 #endif // __UTILS_H__
