aba3b5b402381cbb2e23442f83f121cc876da84a
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2025-04-24 20:35:37 +0100

    arena added for memory management, refactor ASSERT macro and LOG system

--- src/utils.h
+++ src/utils.h
@@ -3,87 +3,109 @@
 
 #include <assert.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
 
 #define UNUSED(VAR)      ((void)(VAR))
-#define IS_POWER_OF_2(x) ((x) > 0 && ((x) & ((x)-1)) == 0)
+#define IS_POWER_OF_2(x) ((x) > 0 && ((x) & ((x) - 1)) == 0)
 
-#define KILOBYTES(number) ((number)*1024ull)
+#define KILOBYTES(number) ((number) * 1024ull)
 #define MEGABYRES(number) (KILOBYTES(number) * 1024ull)
 
-#define SAFE_FREE(POINTER_TO_DATA)    \
-    {                                 \
-        if ((POINTER_TO_DATA))        \
-        {                             \
-            free(POINTER_TO_DATA);    \
-            (POINTER_TO_DATA) = NULL; \
-        }                             \
-    }
-
-// Log levels
-typedef enum
-{
-    LOG_LEVEL_INFO,
-    LOG_LEVEL_WARN,
-    LOG_LEVEL_ERROR
-} log_level_t;
-
-// Helper macro to get the log level as a string
-#define LOG_LEVEL_STR(level) \
-    (level == LOG_LEVEL_INFO ? "INFO" : (level == LOG_LEVEL_WARN ? "WARN" : (level == LOG_LEVEL_ERROR ? "ERROR" : "UNKNOWN")))
-
+//
+// LOGGING
+//
 #define ATTRIBATE_FORMAT_PRINTF(VAL1, VAL2) __attribute__((format(gnu_printf, (VAL1), (VAL2))))
 
-#ifndef NDEBUG
-static inline void _log_printf(log_level_t level, const char *file, int line, const char *fmt, ...) ATTRIBATE_FORMAT_PRINTF(4, 5);
-static inline void _log_printf(log_level_t level, const char *file, int line, const char *fmt, ...)
+static inline void _log_printf(const char *level, const char *fmt, ...) ATTRIBATE_FORMAT_PRINTF(2, 3);
+static inline void _log_printf(const char *level, const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-
-    // fprintf(stdout, "[%s] [%s] (%s:%d) ", time_str, LOG_LEVEL_STR(level), file, line);
-    fprintf(stdout, "[%s](%s:%d) ", LOG_LEVEL_STR(level), file, line);
-
-    // Print the actual log message
-    vfprintf(stdout, fmt, args);
-
-    fflush(stdout); // Flush to ensure the output is immediate
-    va_end(args);
+    va_list va_args;
+    va_start(va_args, fmt);
+    {
+        fprintf(stdout, "%s ", level);
+        vfprintf(stdout, fmt, va_args);
+    }
+    fflush(stdout);
+    va_end(va_args);
 }
 
-    #define LOG(level, fmt, ...) _log_printf(level, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
-    #define LOG_INFO(fmt, ...)   LOG(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
-    #define LOG_WARN(fmt, ...)   LOG(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
-    #define LOG_ERROR(fmt, ...)  LOG(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
-
-    #define ASSERT(expr)                                          \
-        do                                                        \
-        {                                                         \
-            if (!(expr))                                          \
-            {                                                     \
-                fprintf(stderr, "Assertion failed: %s\n", #expr); \
-                abort();                                          \
-            }                                                     \
-        } while (0)
-
-    #define ASSERTM(expr, fmt, ...)                               \
-        do                                                        \
-        {                                                         \
-            if (!(expr))                                          \
-            {                                                     \
-                fprintf(stderr, "Assertion failed: %s\n", #expr); \
-                LOG(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__);         \
-                abort();                                          \
-            }                                                     \
-        } while (0)
-
+#ifndef NDEBUG
+    #define LOG(...)  _log_printf("[LOG]", __VA_ARGS__)
+    #define LOGW(...) _log_printf("[WAR]", __VA_ARGS__)
+    #define LOGE(...) _log_printf("[ERR]", __VA_ARGS__)
 #else
-    #define LOG(level, fmt, ...)
-    #define LOG_INFO(fmt, ...)
-    #define LOG_WARN(fmt, ...)
-    #define LOG_ERROR(fmt, ...)
-
-    #define ASSERT(expr)
-    #define ASSERTM(expr, fmt, ...)
+    #define LOG(...)
+    #define LOGW(...)
+    #define LOGE(...)
 #endif
+
+//
+// ASSERT
+//
+#ifndef NDEBUG
+    #define ASSERT(EXP, ...)  \
+        if (!(EXP))           \
+        {                     \
+            LOG(__VA_ARGS__); \
+            assert(EXP);      \
+        }
+#else
+    #define ASSERT(EXP, ...)
+#endif
+
+//
+// ARENA
+//
+struct arena
+{
+    uint8_t *base;
+    size_t   capacity;
+    size_t   used;
+};
+
+#define MEGABYTE(n) ((n) * (1024 * 1024))
+
+static void *arena_alloc_aligned(struct arena *arena, size_t size, size_t alignment)
+{
+    ASSERT(arena != NULL, "arena is null\n");
+    ASSERT(IS_POWER_OF_2(alignment), "alignment must be a power of 2\n");
+    ASSERT(arena->used + size + alignment - 1 <= arena->capacity, "oom\n");
+
+    uintptr_t ptr         = (uintptr_t)arena->base + arena->used;
+    uintptr_t aligned_ptr = (ptr + alignment - 1) & ~(alignment - 1);
+    size_t    padding     = aligned_ptr - ptr;
+
+    arena->used += size + padding;
+
+    LOG("allocated: %zu (req) + %zu (padding), remaining: %zu\n",
+        size, padding, arena->capacity - arena->used);
+
+    return (void *)aligned_ptr;
+}
+
+static void *ma_push_size(struct arena *arena, const size_t size)
+{
+    ASSERT(arena != NULL, "arena is null");
+    ASSERT(arena->used + size <= arena->capacity, "oom");
+
+    uint8_t *result = arena->base + arena->used;
+    assert(result);
+
+    arena->used += size;
+    return (void *)result;
+}
+
+// main arean will reset after scope exit
+static struct arena tmp_arena(struct arena *a)
+{
+    return (struct arena){
+        .base     = a->base + a->used,
+        .capacity = a->capacity - a->used,
+        .used     = 0,
+    };
+}
+
+#define ma_push_struct(arena, type) (type *)ma_push_size((arena), sizeof(type))
 
 #endif // __UTILS_H__
