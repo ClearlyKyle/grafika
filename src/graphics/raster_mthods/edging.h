70dcc0d04270e84d769e8afb0a17fdc46bcc3971
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2025-04-25 21:14:02 +0100

    udpated edging

--- src/graphics/raster_mthods/edging.h
+++ src/graphics/raster_mthods/edging.h
@@ -3,21 +3,21 @@
 
 #include "common.h"
 
-static void draw_triangle(const triangle_t t)
+static void draw_triangle(const struct triangle t)
 {
-    vec3 screenspace[3] = {0};
-    vec4 clipspace[3]   = {0};
+    vec3 screen_space[3] = {0};
+    vec4 clip_space[3]   = {0};
 
     // convert to clip space
     for (int i = 0; i < 3; ++i)
     {
         vec4 pos = {t.pos[i][0], t.pos[i][1], t.pos[i][2], 1.0f};
-        m4_mul_v4(state.MVP, pos, clipspace[i]);
+        m4_mul_v4(raster_state.MVP, pos, clip_space[i]);
 
         // clipping (is this correct?)
-        const float x = fabsf(clipspace[i][0]);
-        const float y = fabsf(clipspace[i][1]);
-        const float w = fabsf(clipspace[i][3]);
+        const float x = fabsf(clip_space[i][0]);
+        const float y = fabsf(clip_space[i][1]);
+        const float w = fabsf(clip_space[i][3]);
 
         if ((-w <= x && x <= w) || (-w <= y && y <= w))
             continue;
@@ -29,10 +29,10 @@ static void draw_triangle(const triangle_t t)
     vec3 ndc[3] = {0}, w_vals = {0};
     for (size_t i = 0; i < 3; i++)
     {
-        w_vals[i] = 1.0f / clipspace[i][3]; // 1.0f / w
-        ndc[i][0] = clipspace[i][0] * w_vals[i];
-        ndc[i][1] = clipspace[i][1] * w_vals[i];
-        ndc[i][2] = clipspace[i][2] * w_vals[i];
+        w_vals[i] = 1.0f / clip_space[i][3]; // 1.0f / w
+        ndc[i][0] = clip_space[i][0] * w_vals[i];
+        ndc[i][1] = clip_space[i][1] * w_vals[i];
+        ndc[i][2] = clip_space[i][2] * w_vals[i];
     }
 
     // back face culling (surface normal)
@@ -45,25 +45,23 @@ static void draw_triangle(const triangle_t t)
 
     for (int i = 0; i < 3; ++i)
     {
-        screenspace[i][0] = (ndc[i][0] + 1.0f) * 0.5f * GRAFIKA_SCREEN_WIDTH;
-        screenspace[i][1] = (ndc[i][1] + 1.0f) * 0.5f * GRAFIKA_SCREEN_HEIGHT;
-        screenspace[i][2] = (ndc[i][2] + 1.0f) * 0.5f;
+        screen_space[i][0] = (ndc[i][0] + 1.0f) * 0.5f * GRAFIKA_SCREEN_WIDTH;
+        screen_space[i][1] = (ndc[i][1] + 1.0f) * 0.5f * GRAFIKA_SCREEN_HEIGHT;
+        screen_space[i][2] = (ndc[i][2] + 1.0f) * 0.5f;
     }
 
     // calculate bounding rectangle
     int AABB[4] = {0};
-    AABB_make(screenspace, AABB);
+    AABB_make(screen_space, AABB);
 
-    // printf("AABB - minX %d, minY %d, maxX %d, maxY %d\n", AABB[0], AABB[1], AABB[2], AABB[3]);
-
-    float area     = v3_edgefunc(screenspace[0], screenspace[1], screenspace[2]);
+    float area     = v3_edgefunc(screen_space[0], screen_space[1], screen_space[2]);
     float inv_area = 1.0f / area;
 
     // pre fetch tex data
-    const int      texw    = state.tex.w;
-    const int      texh    = state.tex.h;
-    const int      texbpp  = state.tex.bpp;
-    unsigned char *texdata = state.tex.data;
+    const int      tex_w    = raster_state.tex.w;
+    const int      tex_h    = raster_state.tex.h;
+    const int      tex_bpp  = raster_state.tex.bpp;
+    unsigned char *tex_data = raster_state.tex.data;
 
     float *pDepthBuffer = rend.depth_buffer;
 
@@ -74,11 +72,12 @@ static void draw_triangle(const triangle_t t)
         {
             vec3 point = {0.5f + (float)x, 0.5f + (float)y, 0.0f};
 
-            float w0 = v3_edgefunc(screenspace[1], screenspace[2], point);
-            float w1 = v3_edgefunc(screenspace[2], screenspace[0], point);
-            float w2 = v3_edgefunc(screenspace[0], screenspace[1], point);
+            float w0 = v3_edgefunc(screen_space[1], screen_space[2], point);
+            float w1 = v3_edgefunc(screen_space[2], screen_space[0], point);
+            float w2 = v3_edgefunc(screen_space[0], screen_space[1], point);
 
             if (w0 < 0.0f || w1 < 0.0f || w2 < 0.0f)
+                // if ((int)w0 & 0x80000000 || (int)w1 & 0x80000000 || (int)w2 & 0x80000000)
                 continue;
 
             w0 *= inv_area;
@@ -87,7 +86,7 @@ static void draw_triangle(const triangle_t t)
 
             const int index = (x * GRAFIKA_SCREEN_WIDTH) + y;
 
-            const float depth = w0 * screenspace[0][2] + w1 * screenspace[1][2] + w2 * screenspace[2][2];
+            const float depth = w0 * screen_space[0][2] + w1 * screen_space[1][2] + w2 * screen_space[2][2];
 
             float *oldZ = pDepthBuffer + index;
             // const float invZ  = 1.0f / depth;
@@ -112,15 +111,14 @@ static void draw_triangle(const triangle_t t)
             // u = fabsf(u);
             // v = fabsf(v);
 
-            u *= (float)texw - 1;
-            v *= (float)texh - 1;
+            u *= (float)tex_w - 1;
+            v *= (float)tex_h - 1;
 
-            unsigned char *texcolour = texdata + (((int)u + texw * (int)v) * texbpp);
+            unsigned char *tex_colour = tex_data + (((int)u + tex_w * (int)v) * tex_bpp);
 
-            uint32_t pixelcolour = ((uint32_t)0xFFU << 24) |
-                                   ((uint32_t)texcolour[2] << 16) |
-                                   ((uint32_t)texcolour[1] << 8) |
-                                   (uint32_t)texcolour[0];
+            uint32_t pixelcolour = ((uint32_t)tex_colour[0] << 16) |
+                                   ((uint32_t)tex_colour[1] << 8) |
+                                   (uint32_t)tex_colour[2];
 
             // grafika_setpixel(x, y, 0xFFFFFFFF);
             grafika_setpixel(x, y, pixelcolour);
@@ -130,19 +128,73 @@ static void draw_triangle(const triangle_t t)
 
 static void draw_onexit(void) {} /* Do nothing */
 
-static void draw_object(void)
-{
-#pragma omp parallel
-    {
-        obj_t          obj      = state.obj;
-        float         *pPos     = obj.pos;
-        float         *pTex     = obj.texs;
-        vertindices_t *pIndices = obj.indices;
+static struct triangle *triangles = NULL;
 
-#pragma omp for
+static void draw_object(struct arena *arena)
+{
+    obj_t          obj      = raster_state.obj;
+    float         *pPos     = obj.pos;
+    float         *pTex     = obj.texs;
+    vertindices_t *pIndices = obj.indices;
+
+#if 1
+    if (triangles == NULL)
+        triangles = arena_alloc_aligned(arena, sizeof(struct triangle) * obj.num_f_rows, 16);
+
+    #pragma omp parallel
+    {
+    #pragma omp for
+        // gather triangles
+        for (size_t i = 0; i < obj.num_f_rows; ++i)
+        { // CW triangles (which blender uses)
+            vertindices_t indices0   = pIndices[i * 3 + 2];
+            const int     pos_index0 = indices0.v_idx;
+
+            triangles[i].pos[0][0] = pPos[3 * pos_index0 + 0];
+            triangles[i].pos[0][1] = pPos[3 * pos_index0 + 1];
+            triangles[i].pos[0][2] = pPos[3 * pos_index0 + 2];
+
+            const int tex_index0   = indices0.vt_idx;
+            triangles[i].tex[0][0] = pTex[2 * tex_index0 + 0];
+            triangles[i].tex[0][1] = pTex[2 * tex_index0 + 1];
+
+            vertindices_t indices1   = pIndices[i * 3 + 1];
+            const int     pos_index1 = indices1.v_idx;
+
+            triangles[i].pos[1][0] = pPos[3 * pos_index1 + 0];
+            triangles[i].pos[1][1] = pPos[3 * pos_index1 + 1];
+            triangles[i].pos[1][2] = pPos[3 * pos_index1 + 2];
+
+            const int tex_index1   = indices1.vt_idx;
+            triangles[i].tex[1][0] = pTex[2 * tex_index1 + 0];
+            triangles[i].tex[1][1] = pTex[2 * tex_index1 + 1];
+
+            vertindices_t indices2   = pIndices[i * 3 + 0];
+            const int     pos_index2 = indices2.v_idx;
+
+            triangles[i].pos[2][0] = pPos[3 * pos_index2 + 0];
+            triangles[i].pos[2][1] = pPos[3 * pos_index2 + 1];
+            triangles[i].pos[2][2] = pPos[3 * pos_index2 + 2];
+
+            const int tex_index2   = indices2.vt_idx;
+            triangles[i].tex[2][0] = pTex[2 * tex_index2 + 0];
+            triangles[i].tex[2][1] = pTex[2 * tex_index2 + 1];
+        }
+
+    #pragma omp for
+        // drawing triangles
         for (size_t i = 0; i < obj.num_f_rows; ++i)
         {
-            triangle_t t = {0};
+            draw_triangle(triangles[i]);
+        }
+    }
+#else
+    #pragma omp parallel
+    {
+    #pragma omp for
+        for (size_t i = 0; i < obj.num_f_rows; ++i)
+        {
+            struct triangle t;
 
             for (size_t j = 0; j < 3; ++j)
             {
@@ -150,11 +202,11 @@ static void draw_object(void)
 
                 const int posIndex = indices.v_idx;
                 const int texIndex = indices.vt_idx;
-#if 0
-                const size_t storeIndex = j; // CCW triangles
-#else
+    #if 0
+                    const size_t storeIndex = j; // CCW triangles
+    #else
                 const size_t storeIndex = 2 - j; // CW triangles (which blender uses)
-#endif
+    #endif
                 t.pos[storeIndex][0] = pPos[3 * posIndex + 0];
                 t.pos[storeIndex][1] = pPos[3 * posIndex + 1];
                 t.pos[storeIndex][2] = pPos[3 * posIndex + 2];
@@ -165,6 +217,7 @@ static void draw_object(void)
             draw_triangle(t);
         }
     }
+#endif
 }
 
 #endif // __EDGING_H__
